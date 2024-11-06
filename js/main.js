4b7a98720cbead3f05bcfa96122c52be17e02468
lenmkendall <lkendall2@wisc.edu>
2024-11-05 21:10:50 -0500

    changed names

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ window.onload = setMap();
 function setMap() {
     //use Promise.all to parallelize asynchronous data loading
     var promises = [d3.csv("data/unitsData.csv"),
-                    d3.json("data/EuropeCountries.topojson"),
-                    d3.json("data/FranceRegions.topojson")
+                    d3.json("data/EuropeCountries.json"),
+                    d3.json("data/FranceRegions.json")
                     ];
 
             Promise.all(promises).then(callback);
