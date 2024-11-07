74251af74041ed0a74691fb6fe0318eb516d22da
lenmkendall <lkendall2@wisc.edu>
2024-11-06 20:05:33 -0500

    Activity 9

--- js/main.js
+++ js/main.js
@@ -3,11 +3,35 @@ window.onload = setMap();
 
 //set up choropleth map
 function setMap() {
+    
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height)
+
+    //create Albers equal area conic projection centered on France
+    var projection = d3.geoAlbers()
+        .center([0, 46.2])
+        .rotate([-2, 0, 0])
+        .parallels([43, 62])
+        .scale(2500)
+        .translate([width / 2, height / 2]); 
+
+    var path = d3.geoPath()
+        .projection(projection);
+    
     //use Promise.all to parallelize asynchronous data loading
-    var promises = [d3.csv("data/unitsData.csv"),
-                    d3.json("data/EuropeCountries.topojson.json"),
-                    d3.json("data/FranceRegions.topojson.json")
-                    ];
+    var promises = [];
+        
+        promises.push(d3.csv("data/unitsData.csv")); //load attributes from csv
+        promises.push(d3.json("data/EuropeCountries.topojson.json")); //load background spatial data
+        promises.push(d3.json("data/FranceRegions.topojson.json")); //load choropleth spatial data
 
             Promise.all(promises).then(callback);
 
@@ -23,8 +47,24 @@ function setMap() {
             var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
                 franceRegions = topojson.feature(france, france.objects.FranceRegions);
 
+            //add Europe Countries to map
+            var countries = map.append("path")
+                .datum(europeCountries)
+                .attr("class", "countries")
+                .attr("d", path);
+
+            //add France regions to map
+            var regions = map.selectAll(".regions")
+                .data(franceRegions)
+                .enter()
+                .append("path")
+                .attr("class", function(d) {
+                    return "regions " + d.properties.adm1_code;
+                })
+                .attr("d", path);
+
             //examine the results
-            console.log(europeCountries);
-            console.log(franceRegions);
-            };
+            //console.log(europeCountries);
+            //console.log(franceRegions);
+            }; 
 }
